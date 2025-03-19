96a80aecd6a296b6f135e4992d69a801c7843003
carliebosier <carliebosier@localhost>
2025-03-18 23:04:53 -0400

    fixing comment

--- LSP_Spring_2025/src/org/howard/edu/lsp/assignment5/IntegerSet.java
+++ LSP_Spring_2025/src/org/howard/edu/lsp/assignment5/IntegerSet.java
@@ -2,7 +2,7 @@ package org.howard.edu.lsp.assignment5;
 
 /**
  * IntegerSet represents a set of unique integers.
- * It provides methods to add, remove, and perform set operations like union, intersection, etc.
+ * It provides methods to add, remove, and perform set operations like union, intersection
  */
 public class IntegerSet {
 }
