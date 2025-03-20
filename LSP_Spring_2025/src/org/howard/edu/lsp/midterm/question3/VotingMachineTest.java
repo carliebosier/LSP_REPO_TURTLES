df15db708191b5f4f5136829945f48b49bf7a408
Carlie Bosier <carlie.bosier@bison.howard.edu>
2025-03-20 19:51:54 -0400

    name change

--- LSP_Spring_2025/src/org/howard/edu/lsp/midterm/question3/VotingMachineTest.java
+++ LSP_Spring_2025/src/org/howard/edu/lsp/midterm/question3/VotingMachineTest.java
@@ -1,6 +1,6 @@
 package org.howard.edu.lsp.midterm.question3;
 
-public class VotingMachineDriver {
+public class VotingMachineTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
