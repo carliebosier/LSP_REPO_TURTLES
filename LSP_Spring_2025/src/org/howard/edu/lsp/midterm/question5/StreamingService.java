148f627336b408618e11586d25badf0318ce6ae3
Carlie Bosier <carlie.bosier@bison.howard.edu>
2025-03-20 20:54:06 -0400

    completed code for streamingservice class and driver class

--- LSP_Spring_2025/src/org/howard/edu/lsp/midterm/question5/StreamingService.java
+++ LSP_Spring_2025/src/org/howard/edu/lsp/midterm/question5/StreamingService.java
@@ -1,10 +1,108 @@
 package org.howard.edu.lsp.midterm.question5;
 
+/**
+ * class that represents streamable content and their behaviors 
+ */
 public class StreamingService {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    //nested interface to include the different types of media used in this class
+    interface Streamable { //website: https://www.w3schools.com/java/java_interface.asp and https://www.baeldung.com/java-inner-interfaces?utm_source=chatgpt.com
+        void play();
+        void pause();
+        void stop();
+    }
 
-	}
+    //website for nested classes: https://www.geeksforgeeks.org/nested-classes-java/
+    /**
+     * concrete class for music that returns the music, pauses, and adds music to playlist 
+     */
+    class Music implements Streamable { 
+        private String songName;
 
+        public Music(String songName) {
+            this.songName = songName;
+        }
+
+        @Override
+        public void play() {
+            System.out.println("Playing music: " + songName);
+        }
+
+        @Override
+        public void pause() {
+            System.out.println("Paused music: " + songName);
+        }
+
+        @Override
+        public void stop() {
+            System.out.println("Stopped music: " + songName);
+        }
+
+        //adds music to playlist
+        public void addToPlaylist(String playlistName) {
+            System.out.println("Added " + songName + " to " + playlistName + " playlist");
+        }
+    }
+
+    /**
+     * concrete class for movie that returns the movie, pauses, and rewinds
+     */
+    	class Movie implements Streamable {
+        private String movieName;
+
+        public Movie(String movieName) {
+            this.movieName = movieName;
+        }
+
+        @Override
+        public void play() {
+            System.out.println("Playing movie: " + movieName);
+        }
+
+        @Override
+        public void pause() {
+            System.out.println("Paused movie: " + movieName);
+        }
+
+        @Override
+        public void stop() {
+            System.out.println("Stopped movie: " + movieName);
+        }
+
+        //rewinds
+        public void rewind(int seconds) {
+            System.out.println("Rewinding movie: " + movieName + " by " + seconds + " seconds");
+        }
+    }
+
+    	/**
+         * concrete class for audiobook that returns the book title, pauses, and adjusts the playback speed 
+         */
+    	class Audiobook implements Streamable {
+        private String bookTitle;
+
+        public Audiobook(String bookTitle) {
+            this.bookTitle = bookTitle;
+        }
+
+        @Override
+        public void play() {
+            System.out.println("Playing audiobook: " + bookTitle);
+        }
+
+        @Override
+        public void pause() {
+            System.out.println("Paused audiobook: " + bookTitle);
+        }
+
+        @Override
+        public void stop() {
+            System.out.println("Stopped audiobook: " + bookTitle);
+        }
+
+        //sets playback speed
+        public void setPlaybackSpeed(double speed) {
+            System.out.println("Setting playback speed of audiobook: " + bookTitle + " to " + speed + "x");
+        }
+    }
 }
--- LSP_Spring_2025/src/org/howard/edu/lsp/midterm/question5/StreamingServiceDriver.java
+++ LSP_Spring_2025/src/org/howard/edu/lsp/midterm/question5/StreamingServiceDriver.java
@@ -1,10 +1,51 @@
 package org.howard.edu.lsp.midterm.question5;
 
 public class StreamingServiceDriver {
+    public static void main(String[] args) {
+        // Create an instance of the StreamingService (outer class)
+        StreamingService streamingService = new StreamingService();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        // Create instances of different media types using the outer class instance
+        StreamingService.Music music = streamingService.new Music("Still a Friend - Incognito");
+        StreamingService.Movie movie = streamingService.new Movie("Avengers End Game");
+        StreamingService.Audiobook audiobook = streamingService.new Audiobook("A Promised Land – Barack Obama");
 
-	}
+        // Test the common behaviors for all media types (play, pause, stop)
+        System.out.println("Testing common behaviors for all media types:\n");
 
+        // Test Music
+        System.out.println("Testing Music:");
+        music.play();
+        music.pause();
+        music.stop();
+        System.out.println();
+
+        // Test Movie
+        System.out.println("Testing Movie:");
+        movie.play();
+        movie.pause();
+        movie.stop();
+        System.out.println();
+
+        // Test Audiobook
+        System.out.println("Testing Audiobook:");
+        audiobook.play();
+        audiobook.pause();
+        audiobook.stop();
+        System.out.println();
+
+// Test unique behaviors for each media type
+
+        // Test Movie-specific behavior (rewind)
+        movie.rewind(30); //displays in seconds 
+        System.out.println();
+
+        // Test Audiobook-specific behavior (setPlaybackSpeed)
+        audiobook.setPlaybackSpeed(1.5);
+        System.out.println();
+
+        // Test Music-specific behavior (addToPlaylist)
+        music.addToPlaylist("Favorites");
+        System.out.println();
+    }
 }
