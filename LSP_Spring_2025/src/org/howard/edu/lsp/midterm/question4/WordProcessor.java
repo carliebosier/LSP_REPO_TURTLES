0d50dc4a13ca0a152b4c50d6b5ca669b7096d9f6
Carlie Bosier <carlie.bosier@bison.howard.edu>
2025-03-20 20:27:30 -0400

    finished class and driver class

--- LSP_Spring_2025/src/org/howard/edu/lsp/midterm/question4/WordProcessor.java
+++ LSP_Spring_2025/src/org/howard/edu/lsp/midterm/question4/WordProcessor.java
@@ -1,10 +1,46 @@
 package org.howard.edu.lsp.midterm.question4;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *uses a given sentence to identify and return all the longest words
+ */
 public class WordProcessor {
+    private String sentence; 
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public WordProcessor(String sentence) {
+        this.sentence = sentence;
+    }
 
-	}
+    //finds longest words in the sentence
+    //website: https://www.geeksforgeeks.org/program-find-smallest-largest-word-string/
+    public List<String> findLongestWords() {
+        List<String> longestWords = new ArrayList<>();
+        if (sentence == null || sentence.isEmpty()) {
+            return longestWords;
+        }
 
+        //splits sentence into words by spaces
+        //website: https://www.geeksforgeeks.org/split-string-java-examples/
+        String[] words = sentence.trim().split("\\s+");
+        int maxLength = 0;
+
+        //finds the maximum word length
+        for (String word : words) {
+            if (word.length() > maxLength) {
+                maxLength = word.length();
+            }
+        }
+
+        //collects the words that have the maximum length
+        for (String word : words) {
+            if (word.length() == maxLength) {
+                longestWords.add(word);
+            }
+        }
+
+        return longestWords;
+    }
 }
+    
